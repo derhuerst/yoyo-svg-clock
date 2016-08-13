d471c9b2959693bf50e289028cd692118c4a6ac4
Jannis R <mail@jannisr.de>
2016-08-13 09:52:49 +0200

    put everything together

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-// todo
+const yo = require('yo-yo')
+
+const data = require('./data')
+const render = require('./render')
+
+const dom = render(data())
+document.body.appendChild(dom)
+
+let play = true
+const loop = () => {
+	yo.update(dom, render(data()))
+	if (play) requestAnimationFrame(loop)
+}
+requestAnimationFrame(loop)
